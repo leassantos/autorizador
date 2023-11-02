6cf02e72f44c144f59a2529826199101df6dfa21
leassantos <lsts2005@gmail.com>
2023-11-02 06:27:19 -0300

    projeto iniciado

---
+++ src/test/java/com/elumini/desafiovr/AutorizadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elumini.desafiovr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutorizadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
